b8f5141bd1936456541dd50e77ea8f7f5afd2722
SMG <SMG05eoin@gmail.com>
2025-04-30 17:07:06 +0900

    상단메뉴 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Login from "./page/Login";
+import ProductAll from "./page/ProductAll";
+import ProductView from "./page/ProductView";
+import TopMenu from "./component/TopMenu";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="">
+            <TopMenu />
+            <Routes>
+                <Route path={"/"} element={<ProductAll/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/productView/:idx"} element={<ProductView/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/page/ProductView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductView = () => {
+    return (
+        <div>
+           상품 상세 페이지
+        </div>
+    );
+};
+
+export default ProductView;
