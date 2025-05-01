e6c0c0e4e2681b685aacfb144974c755a440c970
SMG <SMG05eoin@gmail.com>
2025-05-01 23:45:31 +0900

    로그인 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Login from "./page/Login";
 import ProductAll from "./page/ProductAll";
 import ProductView from "./page/ProductView";
 import TopMenu from "./component/TopMenu";
+import {useEffect, useState} from "react";
 
 
 function App() {
+    const[auth, setAuth] = useState(false)
+
+    useEffect(() => {
+        console.log("A: ", auth);
+    }, [auth]);
     return (
         <div className="">
-            <TopMenu />
+            <TopMenu auth={auth}/>
             <Routes>
                 <Route path={"/"} element={<ProductAll/>}/>
-                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/login"} element={<Login setAuth={setAuth}/>}/>
                 <Route path={"/productView/:idx"} element={<ProductView/>}/>
             </Routes>
         </div>
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import {Container, Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {useNavigate} from "react-router-dom";
 
-const Login = () => {
+const Login = ({setAuth}) => {
+    const navigate = useNavigate();
+    const loginUser = (event) => {
+        event.preventDefault();
+        setAuth(true);
+        navigate("/");
+    }
+
     return (
-        <div>
-            로그인 페이지
-        </div>
+        <Container>
+            <Form onSubmit={(event)=>loginUser(event)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>이메일</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        귀하의 이메일은 다른 이에게 절대 공유하지 않겠습니다.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>비밀번호</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    로그인
+                </Button>
+            </Form>
+        </Container>
     );
 };
 
