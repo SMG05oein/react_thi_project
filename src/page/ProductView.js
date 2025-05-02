4388d01e5b4d2b5508d5499079296e5eea8657be
SMG <SMG05eoin@gmail.com>
2025-05-02 17:25:20 +0900

    my-json-server

--- src/page/ProductView.js
+++ src/page/ProductView.js
@@ -14,7 +14,7 @@ const ProductView = () => {
 
 
     const getProductView= async ()=>{
-        let url = `https://my-json-server.typicode.com/SMG05eoin/react_thi_project/${id}`
+        let url = `https://my-json-server.typicode.com/SMG05eoin/react_thi_project/products/${id}`
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
